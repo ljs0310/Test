686d299d8e76b071b77982e4a3921b1872ad8726
ljs03101@gmail.com <ljs03101@gmail.com>
2023-06-24 02:11:37 +0900

    기본 설정

---
+++ fProject/src/main/java/com/project/fin/member/controller/MemberController.java
@@ -0,0 +1,31 @@
+package com.project.fin.member.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.project.fin.member.model.service.MemberService;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+	
+	@Autowired
+	private MemberService memberService;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+
+
+}
+
+
+
+
+
+
+
+
+
